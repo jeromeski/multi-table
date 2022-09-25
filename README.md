c6edb30848d85737191bf199f9685b25eff513bd
jeromeski <40538528+jeromeski@users.noreply.github.com>
2022-09-25 16:57:07 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# multi-table
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-brwits)
---
+++ index.js
@@ -0,0 +1,8 @@
+let result = "\n";
+
+for(let i = 1; i < 4; i++) {
+  for(let j = 1; j < 4; j++) {
+    result += i * j + " "
+ };
+  console.log(result)
+};
